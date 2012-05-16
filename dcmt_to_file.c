f50a3fa5145284876c346efce182dfed2cb830e3
Jonathan PASSERAT-PALMBACH <passerat@isima.fr>
2012-05-16 19:31:52 +0200

    Corrected bits number calculation

--- dcmt_to_file.c
+++ dcmt_to_file.c
@@ -17,8 +17,7 @@ int main(void)
 {
    mt_struct *mts = NULL;
 
-   /* This tries to find a small Mersenne Twister with period 2^521-1. */
-   mts = get_mt_parameter_st(sizeof(mt_output_t),19937,4172);
+   mts = get_mt_parameter_st(sizeof(mt_output_t) * 8,19937,4172);
    if (mts == NULL) {
       std::cerr << "error\n" << std::endl;
    }
