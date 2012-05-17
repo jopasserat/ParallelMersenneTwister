5ec1b565b043006d13292e9c95c55a3699709af3
Jonathan PASSERAT-PALMBACH <passerat@isima.fr>
2012-05-17 18:19:41 +0200

    Enabled mersenne exponent and file name parameters to be passed on the command line.

--- dcmt_to_file.c
+++ dcmt_to_file.c
@@ -1,6 +1,9 @@
-/*
-   This is a simple example for Dynamic Creator library.
- */
+/**
+   @file dcmt_to_file.c
+   Calls Dynamic Creator and stores its results into a CSV file.
+
+   @author Jonathan PASSERAT-PALMBACH
+*/
 
 #include <cstdio>
 #include <cstdlib>
@@ -13,8 +16,26 @@ extern "C" {
 
 typedef uint32_t mt_output_t;
 
-int main(void)
+void usage(const std::string& binaryName) {
+   std::cerr << "Usage:\n\t" << binaryName << " <mersenne exponent> <output file name>" << std::endl;
+}
+
+int main(int argc, char* argv[])
 {
+   if (argc != 3) {
+      usage(argv[0]);
+      exit(1);
+   }
+   
+   int mersenneExponent;
+   std::string filename(argv[2]);
+
+   if ( sscanf(argv[1], "%d", &mersenneExponent) != 1 ||
+        filename.empty() ) {
+      usage(argv[0]);
+      exit(2);
+   }
+ 
    mt_struct *mts = NULL;
 
    mts = get_mt_parameter_st(sizeof(mt_output_t) * 8,19937,4172);
